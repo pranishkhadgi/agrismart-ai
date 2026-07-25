a503d863c23aa047b9edb585148df62d388e21e5
Pranish Khadgi <pranishkhadgi65@gmail.com>
2026-07-25 11:31:41 +0545

    Add render config and requirements

--- app.py
+++ app.py
@@ -92,4 +92,4 @@ def predict():
         return f"<h3>Error: {str(e)}</h3><a href='/'>Go back</a>"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
